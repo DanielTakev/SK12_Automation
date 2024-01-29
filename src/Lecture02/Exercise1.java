e5a60a3ae9689bec02d9e3ceea7bd129337cb413
Daniel Takev <daniel.takev.vm@gmail.com>
2024-01-29 21:35:24 +0200

    Added project

---
+++ src/Lecture02/Exercise1.java
@@ -0,0 +1,14 @@
+package Lecture02;
+
+public class Exercise1 {
+    // Store your names in 3 different variables and print your whole name
+    public static void main(String[] args) {
+        String firstName = "Daniel";
+        String middleName = "Emilov";
+        String lastName = "Takev";
+
+        System.out.println("First name: " + firstName);
+        System.out.println("Middle name: " + middleName);
+        System.out.println("Last name: " + lastName);
+    }
+}
